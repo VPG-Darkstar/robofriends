40556399ba3a6ed2f16a3cf64b5515d83ccc55e8
Evan Choquette <echoquette97@gmail.com>
2018-11-08 18:10:29 -0500

    Add code from Complete Junior to Senior Web Dev course

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Berta Kadlec',
+		username: 'terriwavelength',
+		email: 'kewley@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Léandre Derrickson',
+		username: 'gildlend99',
+		email: 'djupedal@optonline.net'
+	},
+	{
+		id: 3,
+		name: 'Zulaykha Blackbourne',
+		username: 'pover_chord',
+		email: 'ghost@me.com'
+	},
+	{
+		id: 4,
+		name: 'Annagül Coello',
+		username: 'blazinglemons800',
+		email: 'quantaman@yahoo.ca'
+	},
+	{
+		id: 5,
+		name: 'Magdalena Hyland',
+		username: 'piment0safe',
+		email: 'gtewari@mac.com'
+	},
+	{
+		id: 6,
+		name: 'Alhaji Kertész',
+		username: 'boliegineornium',
+		email: 'payned@yahoo.com'
+	},
+	{
+		id: 7,
+		name: 'Alondra Beaumont',
+		username: 'sable_midsorthoa483',
+		email: 'froodian@live.com'
+	},
+	{
+		id: 8,
+		name: 'Meagan Lazarov',
+		username: 'economicmaggie1010',
+		email: 'eminence@att.net'
+	},
+	{
+		id: 9,
+		name: 'Harel Rice',
+		username: 'elkin-rage-666',
+		email: 'cderoove@verizon.net'
+	},
+	{
+		id: 10,
+		name: 'Hesekiel Schäfer',
+		username: 'amberstiownsick',
+		email: 'magusnet@yahoo.com'
+	}
+]
